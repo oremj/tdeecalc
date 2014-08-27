8a22eaa8f733b1f78ff1dbae80dc6557b504118b
Jeremiah Orem <oremj@oremj.com>
2014-08-26 17:07:59 -0700

    web: initial

---
+++ web/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tdeecalcApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tdeecalcApp
+ */
+angular.module('tdeecalcApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
